f1975a26c2b6d3347e84b38c07ea3fa2ae213bbf
Alexey Lazarev <googlaz1@gmail.com>
2020-01-29 23:39:36 +0300

    ООП. Начало

--- caesar_encryptor.py
+++ caesar_encryptor.py
@@ -3,9 +3,7 @@
 
 # Дан русский алфавит.
 
-alphabet = [
-    'абвгдеёжзийклмнопрстуфхцчшщъыьэюя',
-]
+import alphabet_settings
 
 # Необходимо написать программу, шифрующее слово с помощью Шифра Цезаря(https://is.gd/rcGAsp).
 # Программа должна принимать на вход от пользователя слово и число сдвигов.
@@ -30,82 +28,91 @@ alphabet = [
 # Число сдвигов:  3
 # Вывод:          фнлоодснф
 
-message = input('Введите слово: ')
-shift = int(input('Введите сдвиг: '))
-
 # --------------------------------------------------------------------------------
-# MY DECISION AT THIS STAGE:
 
-# CODER
 
-for element in range(shift):
-    shift_alphabet = alphabet[0][element + 1:] + alphabet[0][:element + 1]
-    if element > 31:
-        if element == 32:   # Не нравится мне этот метод решения проблемы shift > числа элементов списка :(
-            alphabet.append(shift_alphabet) # ух как не нравится!
+class CaesarEncrypt:
+
+    def __init__(self, word, shift, state='encode', alphabet='ru'):
+        self.state = state
+        self.alphabet = alphabet_settings.alphabet  # temporarily hard decision
+        self.user_word = word
+        self.shift = shift
+
+    def run(self):
+        if self.state == 'encode':
+            self.encoder()
         else:
-            shift_alphabet = alphabet[0][(element % 32):] + alphabet[0][:(element % 32)] #фууу
-            alphabet.append(shift_alphabet)
-            continue
-    alphabet.append(shift_alphabet)
+            self.decoder()
 
-index = 0
-secret_word = ''
-for letter in message:
-    for char in alphabet[0]:
-        index += 1
-        new_letter = char == letter
-        if new_letter:
-            new_message = alphabet[shift][index - 1]
-            secret_word += new_message
-            index = 0
-            break
-        else:
-            continue
+    def encoder(self):
+        for element in range(self.shift):
+            shift_alphabet = self.alphabet[0][element + 1:] + self.alphabet[0][:element + 1]
+            if element > 31:
+                if element == 32:  # Не нравится мне этот метод решения проблемы shift > числа элементов списка :(
+                    self.alphabet.append(shift_alphabet)  # ух как не нравится!
+                else:
+                    shift_alphabet = self.alphabet[0][(element % 32):] + self.alphabet[0][:(element % 32)]  # фууу
+                    self.alphabet.append(shift_alphabet)
+                    continue
+            self.alphabet.append(shift_alphabet)
 
-print(secret_word)
+        index = 0
+        secret_word = ''
+        for letter in self.user_word:
+            for char in self.alphabet[0]:
+                index += 1
+                if char == letter:
+                    new_message = self.alphabet[self.shift][index - 1]
+                    secret_word += new_message
+                    index = 0
+                    break
+                else:
+                    continue
 
-# ------------------------------------------------
-# DECODER.
-#
-# Необходимо написать программу, которая расшифровывает заданное зашифрованное слово силами Шифра Цезаря
-# Подробнее о шифре Цезаря - (https://is.gd/rcGAsp).
-# Дано зашифрованное сообщение, число сдвигов и русский алфавит
-#
-# Пример:
-# message_encrypt = 'фхнжйч'
-# shift_encrypt = 5
-# Вывод: Зашифрованным словом было - привет
+        print(secret_word)
 
-# -----------------------------------------------
+    def decoder(self):
+        # DECODER.
+        # Необходимо написать программу, которая расшифровывает заданное зашифрованное слово силами Шифра Цезаря
+        # Подробнее о шифре Цезаря - (https://is.gd/rcGAsp).
+        # Дано зашифрованное сообщение, число сдвигов и русский алфавит
 
-# КОД программы
+        """
+         Пример:
+        # message_encrypt = 'фхнжйч'
+        # shift_encrypt = 5
+        # Вывод: Зашифрованным словом было - привет
+        """
 
-# message_encrypt = 'фнлоодснф'
-# shift_encrypt = 3
-#
-# for element in range(shift):
-#     shift_alphabet = alphabet[0][element + 1:] + alphabet[0][:element + 1]
-#     alphabet.append(shift_alphabet)
-#
-#
-# index = 0
-# secret_word = ''
-# for letter in message_encrypt:
-#     for char in alphabet[shift_encrypt]:
-#         index += 1
-#         new_letter = char == letter
-#         if new_letter:
-#             new_message = alphabet[0][index - 1]
-#             secret_word += new_message
-#             index = 0
-#             break
-#         else:
-#             continue
-#
-# print('Зашифрованным словом было -', secret_word)
+        for element in range(self.shift):
+            shift_alphabet = self.alphabet[0][element + 1:] + self.alphabet[0][:element + 1]
+            self.alphabet.append(shift_alphabet)
+
+        index = 0
+        secret_word = ''
+        for letter in self.user_word:
+            for char in self.alphabet[self.shift]:
+                index += 1
+                new_letter = char == letter
+                if new_letter:
+                    new_message = self.alphabet[0][index - 1]
+                    secret_word += new_message
+                    index = 0
+                    break
+                else:
+                    continue
+
+        print('Зашифрованным словом было -', secret_word)
+
+
+if __name__ == "__main__":
+    young_encryptor = CaesarEncrypt(word=input('Введите слово: '),
+                                    shift=int(input('Введите сдвиг: ')),
+                                    state='decode')
+    young_encryptor.run()
+
 
 # TODO ввести проверку принадлежности символа алфавиту
 # TODO включить возможность ввода целого предложения с пробелами и знаками препинания (которые тоже будут шифроваться)
 # TODO добавить английский алфавит
-# TODO переписать на классах
