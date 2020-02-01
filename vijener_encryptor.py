ecc2cfb7da7205808e01c7607e9c0fe94cf546bf
Alexey Lazarev <googlaz1@gmail.com>
2020-02-01 21:23:30 +0300

    декодер добавлен. Не забывать причесывать код. Написать тесты

--- vijener_encryptor.py
+++ vijener_encryptor.py
@@ -3,16 +3,7 @@
 
 # Дан словарь русского алфавита.
 
-alphabet_ru = {
-    'а': 0, 'б': 1, 'в': 2, 'г': 3, 'д': 4, 'е': 5,
-    'ё': 6, 'ж': 7, 'з': 8, 'и': 9, 'й': 10, 'к': 11,
-    'л': 12, 'м': 13, 'н': 14, 'о': 15, 'п': 16, 'р': 17,
-    'с': 18, 'т': 19, 'у': 20, 'ф': 21, 'х': 22, 'ц': 23,
-    'ч': 24, 'ш': 25, 'щ': 26, 'ь': 27, 'ъ': 28, 'э': 29,
-    'ю': 30, 'я': 31,
-}
-
-ru = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+import alphabet_settings
 
 
 # Необходимо написать программу, шифрующее слово с помощью Шифра Виженера (https://is.gd/WEVeME) и
@@ -38,107 +29,61 @@ ru = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
 # -------------------------------------------------------------------------
 
-# # MY DECISION AT THIS STAGE: Кодер шифра Виженера для русского алфавита
-
-# message = input('Введите слово: ')
-# key_message = input('Введите ключ: ')
-# letter_ru = (list(alphabet_ru.keys()))
-
-import time
-
-
-class Profiler(object):
-    def __enter__(self):
-        self._startTime = time.time()
-
-    def __exit__(self, type, value, traceback):
-        print("Elapsed time: {:.3f} sec".format(time.time() - self._startTime))
-
-
 class VijenerEnc:
 
-    def __init__(self, user_word, key_word, alphabet):
+    def __init__(self, user_word, key_word, mode='encode', alphabet='ru'):
         self.user_word = user_word
         self.key_word = key_word
+        self.mode = mode
         self.alphabet = alphabet
-        self.index_word = []
-        self.index_key = []
 
-    def find_index(self, my_str, symbol):
+    def _find_index(self, my_str, symbol, inverse=False):
         for i in symbol:
             start = my_str.find(i)
-            yield start
+            if inverse:
+                yield 0 - start
+            else:
+                yield start
             start += len(symbol)
 
-    def get_key(self, value):
-        for num in value:
-            yield ru[num]
-
     def run(self):
-        with Profiler() as p:
-            total_index_1 = self.find_index(ru, self.user_word)
-            total_index_2 = self.find_index(ru, self.key_word)
-            a = [(i + j) % len(self.alphabet) for i, j in zip(total_index_1, total_index_2)]
-            for i in self.get_key(a):
-                print(i, end='')
+        if len(self.user_word) > len(self.key_word):
+            self.key_word += self.key_word[:len(self.user_word) - len(self.key_word)]
+        total_index_1 = self._find_index(self.alphabet, self.user_word)
+
+        if self.mode == 'decode':
+            total_index_2 = self._find_index(self.alphabet, self.key_word, inverse=True)
+        else:
+            total_index_2 = self._find_index(self.alphabet, self.key_word)
+
+        for index_word, index_key in zip(total_index_1, total_index_2):
+            print(self.alphabet[(index_word + index_key) % len(self.alphabet)], end='')
+
+
+# permutation = [(index_word - index_key) % len(self.alphabet)
+#               for index_word, index_key in zip(total_index_1, total_index_2)]
 
 
 if __name__ == "__main__":
     young_encryptor = VijenerEnc(user_word=input('Введите слово: '),
                                  key_word=input('Введите сдвиг: '),
-                                 alphabet=ru)
+                                 mode='decode',
+                                 alphabet=alphabet_settings.ru)
     young_encryptor.run()
 
 # ----------------------------------------------------------------------
-# декодер шифра Виженера
-
-#
-# message = input('Введите слово: ')
-# key_message = 'скиллбокс'
-# letter_rus = (list(alphabet_ru.keys()))
-#
-#
-# def get_key(alphabet_rus, value):
-#     for key, number in alphabet_ru.items():
-#         if number == value:
-#             return key
-#
-#
-# if len(message) > len(key_message):
-#     index_key_message = 0
-#     for char in range(len(message) - len(key_message)):
-#         key_message += key_message[index_key_message]
-#         index_key_message += 1
-#
-#
-# new_letter = []
-# new_key_letter = []
-# for i2, key_letter in enumerate(key_message):
-#     for char in letter_rus:
-#         number_letter_key = char == key_letter
-#         if number_letter_key:
-#             new_key_letter.append(alphabet_ru.get(char))
-#             break
-#
-# for i1, letter_message in enumerate(message):
-#     for char in letter_rus:
-#         number_letter = char == letter_message
-#         if number_letter:
-#             new_letter.append(alphabet_ru.get(char))
-#             break
-#
-# initial_word = ''
-# for index_initial_word, letter_initial_word in enumerate(new_letter):
-#     initial_word_number = letter_initial_word + 32 - new_key_letter[index_initial_word]
-#     if initial_word_number > 31:
-#         initial_word += get_key(alphabet_ru, initial_word_number % 32)
-#     else:
-#         initial_word += get_key(alphabet_ru, initial_word_number)
-#         continue
-# print(initial_word)
-
 
 # TODO дан словарь из английского алфавита. Попробовать кодер Виженера
+
+# alphabet_ru = {
+#     'а': 0, 'б': 1, 'в': 2, 'г': 3, 'д': 4, 'е': 5,
+#     'ё': 6, 'ж': 7, 'з': 8, 'и': 9, 'й': 10, 'к': 11,
+#     'л': 12, 'м': 13, 'н': 14, 'о': 15, 'п': 16, 'р': 17,
+#     'с': 18, 'т': 19, 'у': 20, 'ф': 21, 'х': 22, 'ц': 23,
+#     'ч': 24, 'ш': 25, 'щ': 26, 'ь': 27, 'ъ': 28, 'э': 29,
+#     'ю': 30, 'я': 31,
+# }
+
 
 # alphabet_eng = {
 #     'a': 0,  'b': 1,  'c': 2,  'd': 3,  'e': 4,  'f': 5,
