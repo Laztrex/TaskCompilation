c8969d7c62cfbb3aef68ccfb8f52d4d67d924227
Alexey Lazarev <googlaz1@gmail.com>
2020-02-02 17:15:26 +0300

    init unittests. продолжить модифицировать argparse

---
+++ tests/test_correct_encryption.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import Mock
+from termcolor import cprint
+
+from caesar_encryptor import CaesarEncrypt
+from vijener_encryptor import VijenerEnc
+
+
+class GlobalScoreTest(unittest.TestCase):
+    pass
